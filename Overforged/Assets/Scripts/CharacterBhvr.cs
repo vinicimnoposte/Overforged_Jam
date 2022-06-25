7fc6a79bf0fee837f8553365bd2d3a0bd8bb19a9
Vinícius Januzzi <mainlee684@gmail.com>
2022-06-25 02:43:01 -0300

    3 skill checks
    multi bar press, a~d, dbd skillcheck com falha e acerto

--- Overforged/Assets/Scripts/CharacterBhvr.cs
+++ Overforged/Assets/Scripts/CharacterBhvr.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 public class CharacterBhvr : MonoBehaviour
 {
     public float MovementSpeed = 1f;
-   
+    public GameObject puzzleSpace;
+    public GameObject puzzleArrow;
+
+    private void Start()
+    {
+        puzzleSpace.SetActive(false);
+        puzzleArrow.SetActive(false);
+    }
     private void Update()
     {
         var movementH = Input.GetAxis("Horizontal");
         var movementV = Input.GetAxis("Vertical");
         transform.position += new Vector3(movementH, movementV, 0) * Time.deltaTime * MovementSpeed;
+        if(Input.GetKeyDown("e"))
+        {
+            puzzleSpace.SetActive(true);
+        }
+        if(Input.GetKeyDown("q"))
+        {
+            puzzleArrow.SetActive(true);
+        }
     }
 }
--- Overforged/Assets/Scripts/Skillcheck.cs
+++ Overforged/Assets/Scripts/Skillcheck.cs
@@ -4,44 +4,73 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Skillcheck : MonoBehaviour
 {
+    #region Variaveis
+    GameObject player;
+    public GameObject shrekpai;
+    public GameObject fionapai;
     public Image shrek;
+    public Image fiona;
     public float carrega = 0;
-    public bool coolingDown;
+    public bool coolingDownS = false;
+    public bool coolingDownF;
     public float waitTime = 10.0f;
     public float sobe;
-    // Start is called before the first frame update
-    void Start()
+    public bool pressA;
+    public bool pressD;
+    #endregion
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if(coolingDown == true)
+        if(coolingDownS == true)
         {
             shrek.fillAmount -= 1.0f / waitTime * Time.deltaTime;
         }
-       // shrek.fillAmount = carrega;
-       // if (shrek.fillAmount > 0)
-            
-
-        // carrega += Time.deltaTime;
-        // if(carrega == 1)
-        // {
-        //
-        // }
         if (Input.GetKeyDown("space"))
         {
-            coolingDown = true;
+            coolingDownS = true;
             shrek.fillAmount += sobe;
         }
         if(shrek.fillAmount == 1)
         {
-            coolingDown = false;
-            print("Felicidades");
+            coolingDownS = false;
+            print("Shrek sucesso");
+            shrekpai.SetActive(false);
         }
-        //if(Input.GetKeyDown(""))
         
+        
+        if(Input.GetKeyDown("a") && !pressA)
+        {
+            pressA = true;
+            pressD = false;
+            coolingDownF = true;
+            fiona.fillAmount += sobe;
+        }
+        if(Input.GetKeyDown("d") && !pressD)
+        {
+            pressA = false;
+            pressD = true;
+            coolingDownF = true;
+            fiona.fillAmount += sobe;
+        }
+        if(coolingDownF == true)
+        {
+            fiona.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+        }
+        if(fiona.fillAmount >= 0.95f)
+        {
+            coolingDownF = false;
+            print("Fiona sucesso");
+            fionapai.SetActive(false);
+        }
+        if(shrekpai.activeInHierarchy || fionapai.activeInHierarchy)
+        {
+            //GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterBhvr>().enabled = false;
+            player.GetComponent<CharacterBhvr>().enabled = false;
+        }
+        else
+            player.GetComponent<CharacterBhvr>().enabled = true;
     }
 }
